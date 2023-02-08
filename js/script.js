724a4c2a1f5ceb6bd8d5a2b7890aa44b0e07b70a
Patrizia Busatto <patribu7@gmail.com>
2023-02-08 16:46:59 +0100

    Riordinate le carte

--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
-deck = ['python', 'javascript', 'html5', 'css3', 'qualche altra cosa']
+deckDiv = document.getElementById("deck");
+deckProgrammer = ['python', 'javascript', 'html5', 'css3'];
+deckDesigner = ['photoshop', 'inkscape', 'procreate'];
 
-
-function addToDeck(skill) {
+function addToDeck(skill, type, num = 0) {
     var card = document.createElement("div");
-    card.className = "card";
+    // aggiungi due classi alla card
+    card.classList.add('card', type);
 
-    //aggiungo la prima immagine
-    img = card.appendChild(document.createElement('img'));
-    img.src = 'img/cards/' + skill + '.svg';
-
+    firstDiv = card.appendChild(document.createElement('div'));
+    firstNum = firstDiv.appendChild(document.createElement('p'));
+    firstNum.innerHTML = num;
+    firstImg = firstDiv.appendChild(document.createElement('img'));
+    firstImg.src = 'img/cards/' + skill + '.svg';
+        
     //aggiungo il nome della skill
     card.appendChild(document.createElement('p')).innerHTML = skill;
 
-    // aggiungo la seconda immagine
-    img = card.appendChild(document.createElement('img'));
-    img.src = 'img/cards/' + skill + '.svg';
+    lastDiv = card.appendChild(document.createElement('div'));
+    lastNum = lastDiv.appendChild(document.createElement('p'));
+    lastNum.innerHTML = num;    
+    lastImg = lastDiv.appendChild(document.createElement('img'));
+    lastImg.src = 'img/cards/' + skill + '.svg';
 
 
-    document.getElementById("deck").appendChild(card);
+    deckDiv.appendChild(card);
 
-}
+};
 
-deck.forEach(card => {
-    addToDeck(card);
+deckProgrammer.forEach(card => {
+    addToDeck(card, 'programmazione');
     
 });
 
-
+deckDesigner.forEach(card => {
+    addToDeck(card, 'design');
+});
